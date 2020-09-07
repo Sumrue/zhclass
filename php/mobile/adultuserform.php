86c0ff5569f8643c3a448cd9810dccc7dbed5364
sumrue <907870135@qq.com>
2020-09-07 09:38:45 +0800

    表单规则完善，数据渲染

--- php/mobile/adultuserform.php
+++ php/mobile/adultuserform.php
@@ -1,4 +1,42 @@
 <?php
-// 成人高考优惠活动1页面PHP
+/*
+ * @Description:
+ * @Author: Ophites
+ * @Date: 2020-09-05 13:49:14
+ * @LastEditors: Ophites
+ * @LastEditTime: 2020-09-05 14:28:41
+ */
+$at=$_GPC['at'];
+if (empty($at)) {
+    $school = pdo_fetchall("SELECT * FROM " .tablename("fy_lesson_school"). " WHERE uniacid=:uniacid order by displayorder desc,id desc", array(':uniacid'=>$uniacid));
+    foreach ($school as $key=>$item) {
+        $lid=json_decode($item['lid'], true);
+        $level=[];
+        foreach ($lid as $litem) {
+            $level[] = pdo_fetch("SELECT id,name FROM " .tablename("fy_lesson_school_level"). " WHERE uniacid=:uniacid and id=:id", array(':uniacid'=>$uniacid,'id'=>$litem));
+        }
+        $school[$key]['lid']=$level;
+    }
+} elseif ($at=='pro') {
+    $id=$_GPC['id'];
+    $lid=$_GPC['lid'];
+    $pro = pdo_fetchall("SELECT s.id,p.name,s.area FROM " .tablename("fy_lesson_school_spec"). " as s left join " .tablename("fy_lesson_school_pro"). " as p on p.id=s.pid where s.uniacid=:uniacid and s.sid=:id and p.lid=:lid", array(':uniacid'=>$uniacid,':id'=>$id,':lid'=>$lid));
+    foreach ($pro as $key=>$item) {
+        $area_list=json_decode($item['area'], true);
+        $area=[];
+        foreach ($area_list as $aitem) {
+            if (!isset($area[$aitem['cid']])) {
+                $area[$aitem['cid']]=[];
+                $tmp=pdo_fetch("SELECT ext_name FROM " .tablename("fy_lesson_school_addr"). " WHERE id=:id", array('id'=>$aitem['cid']));
+                $area[$aitem['cid']]['name']=$tmp['ext_name'];
+                $area[$aitem['cid']]['tid']=[];
+            }
+            $tmp=pdo_fetch("SELECT ext_name FROM " .tablename("fy_lesson_school_addr"). " WHERE id=:id", array('id'=>$aitem['tid']));
+            $area[$aitem['cid']]['tid'][$aitem['tid']]=$tmp['name'];
+        }
+        $pro[$key]['area']=$area;
+    }
+    $this->resultJson($pro);
+}
+
 include $this->template("../mobile/{$template}/adultuserform");
-?>
--- php/mobile/schooldetail.php
+++ php/mobile/schooldetail.php
@@ -1,15 +1,21 @@
 <?php
-/**
- * 讲师列表
- * ============================================================================
- * 版权所有 2015-2020 风影科技，并保留所有权利。
- * 网站地址: https://www.fylesson.com
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件，未购买授权用户无论是否用于商业行为都是侵权行为！
- * 允许已购买用户对程序代码进行修改并在授权域名下使用，但是不允许对程序代码以
- * 任何形式任何目的进行二次发售，作者将依法保留追究法律责任的权力和最终解释权。
- * ============================================================================
+/*
+ * @Description:
+ * @Author: Ophites
+ * @Date: 2020-09-02 10:03:43
+ * @LastEditors: Ophites
+ * @LastEditTime: 2020-09-05 09:12:14
  */
-
+$id = intval($_GPC['id']);
+if (empty($id)) {
+    message("该学校不存在或已被删除！", "", "error");
+}
+$school = pdo_fetch("SELECT * FROM " .tablename("fy_lesson_school"). " WHERE uniacid=:uniacid AND id=:id", array(':uniacid'=>$uniacid, ':id'=>$id));
+if (empty($school)) {
+    message("该学校不存在或已被删除！", "", "error");
+}
+$school["spec"] = pdo_fetch("SELECT * FROM " .tablename("fy_lesson_school_spec"). " WHERE uniacid=:uniacid AND sid=:sid", array(':uniacid'=>$uniacid, ':sid'=>$id));
+foreach ($school["spec"] as $key=>$item) {
+    $school["spec"][$key]['area']=json_decode($item['area'], true);
+}
 include $this->template("../mobile/{$template}/schooldetail");
-?>
